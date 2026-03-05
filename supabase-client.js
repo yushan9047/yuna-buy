eb72cb5234311a3505ff9a013d2bc55765a45b69
yushan9047 <96381014+yushan9047@users.noreply.github.com>
2026-03-05 11:52:05 +0800

    Update supabase-client.js

--- supabase-client.js
+++ supabase-client.js
@@ -1,7 +1,7 @@
 // supabase-client.js
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 
-export const SUPABASE_URL = "YOUR_SUPABASE_URL";
+export const SUPABASE_URL = "https://iaiobzgajtbllszpllbx.supabase.co";
 export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlhaW9iemdhanRibGxzenBsbGJ4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI2NjIyMTAsImV4cCI6MjA4ODIzODIxMH0.UeAYQib1ZQ8l1WzGmT4Bg_hOIAOPRhZ6JbPQYug0GN0";
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
